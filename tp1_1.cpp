149d408976dd3d93def7361de64565411eef87a2
gerardogonzalez227 <gerardogonzalez227@hotmail.com>
2019-03-21 00:29:14 -0300

    se agrega respuesta a NOTA

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -25,3 +25,13 @@ int main(){
 
 	return 0;
 }
+/*
+Nota: Si resolvió correctamente los puntos 2 y 3 notará que el resultado es el mismo. ¿a qué se debe?
+¿Qué obtiene en el punto 4? ¿es igual a los anteriores? ¿por qué?
+
+ En los puntos mencionados el resultado es el mismo,
+ pues el puntero guarda la direccion de memoria de la variable a la cual apunta, En mi caso
+ a la variable a. 
+ En el punto 4 los resultados son diferentes, la variable a tiene una diferente direccion
+ de memoria que el puntero p
+*/
